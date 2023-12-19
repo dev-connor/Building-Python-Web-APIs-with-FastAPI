a200541eace120ec06879d5c86fc2141b7178e7d
Connor <kor.connor@gmail.com>
2023-12-19 12:40:34 +0900

    [Feature] 이벤트 라우터 작성

--- planner/models/events.py
+++ planner/models/events.py
@@ -1,13 +1,31 @@
-from pydantic import BaseModel
-from typing import List
+from beanie import Document
+from typing import Optional, List
 
-class Event(BaseModel):
-    id: int
+class Event(Document):
     title: str
     image: str
     description: str
     tags: List[str]
     location: str
+
+    class Config:
+        json_schema_extra = {
+            'example': {
+                'title': 'FastAPI Book Launch',
+                'image': 'https://linktomyimage.com/image.png',
+                'description': 'We will be discussing the contents of the FastAPI book in this event...',
+                'tags': ['python','fastapi','book','launch'],
+                'location': 'Google Meet',
+            }
+        }
+    class Settings:
+        name = 'events'
+class EventUpdate(Document):
+    title: Optional[str]
+    image: Optional[str]
+    description: Optional[str]
+    tags: Optional[List[str]]
+    location: Optional[str]
 
     class Config:
         json_schema_extra = {
